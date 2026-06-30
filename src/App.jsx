904789d30f9af0d5ba33bb1b99f33b3c7bb6a767
thanhvan4433 <van.pt0404@gmail.com>
2026-06-30 14:31:00 +0700

    fix navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Specifications from "./components/Specifications";
